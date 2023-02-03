1d50051e297a4cafff4fba276faac1d3c5f96bbb
Guilherme Aiex <guilhermemedeirosaiex@gmail.com>
2023-02-03 00:28:09 -0300

    first_commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopComponent } from './top/top.component';
+import { PainelComponent } from './painel/painel.component';
+import { TentativasComponent } from './tentativas/tentativas.component';
+import { ProgressoComponent } from './progresso/progresso.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopComponent,
+    PainelComponent,
+    TentativasComponent,
+    ProgressoComponent,
   ],
   imports: [
     BrowserModule,
