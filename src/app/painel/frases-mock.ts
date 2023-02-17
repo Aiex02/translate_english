28057107cca55081ba0a56434816a01737d0fccd
Guilherme Aiex <guilhermemedeirosaiex@gmail.com>
2023-02-16 22:53:52 -0300

    v 1.0

---
+++ src/app/painel/frases-mock.ts
@@ -0,0 +1,8 @@
+import { Frase } from '../shared/frase.model'
+
+export const FRASES: Frase[]= [
+    { fraseEng: 'I like to learn', frasePtBr: 'Eu gosto de aprender' },
+    { fraseEng: 'I love my tiny dog', frasePtBr: 'Eu amo meu cachorro pequeno' },
+    { fraseEng: 'I whatch tv', frasePtBr: 'Eu assisto tv' },
+    { fraseEng: 'I eat bread', frasePtBr: 'Eu como pão' }
+]
