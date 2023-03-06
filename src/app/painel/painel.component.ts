b27ac5bcfd984e0f2c67ed3498e51e909d5caaf5
Guilherme Aiex <guilhermemedeirosaiex@gmail.com>
2023-03-06 10:31:02 -0300

    v 0.2

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -12,13 +12,19 @@ export class PainelComponent implements OnInit {
 
   public frases: Frase[]= FRASES
   public instruction: string = 'Traduza a frase:'
+  public resposta!: string; 
 
   constructor() { console.log(this.frases)}
 
   ngOnInit(): void {
   }
   
-  atualizaResposta(): void {
-    console.log('teste')
+  public atualizaResposta(resposta:Event): void {
+    this.resposta = ((<HTMLInputElement>resposta.target).value)
+    console.log(this.resposta)
+  }
+
+  public verificarResposta(): void {
+    console.log("Verificar resposta", this.resposta)
   }
 }
